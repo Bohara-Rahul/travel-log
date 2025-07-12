85c056bcb7b3c586dc3313393f72cd4bfdc4860e
Rahul Bohara <rahulbohara@Rahuls-MacBook-Pro.local>
2025-07-13 02:28:09 +0800

    Guest redirect to login page done

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,19 @@
+<script lang="ts" setup>
+const authStore = useAuthStore();
+</script>
+
 <template>
   <div class="hero bg-base-300 container mx-auto">
     <div class="hero-content text-center my-80">
       <div class="max-w-md">
         <h1 class="text-5xl font-bold">Travel Log</h1>
         <p class="py-6">
-          Document you travel log history
+          Keep track of your travels and adventures with this simple travel log app. Add 
         </p>
-        <AuthButton />
+        <AuthButton v-if="!authStore.user" />
+        <NuxtLink to="/dashboard" class="btn btn-primary" v-else>
+          Start Logging
+        </NuxtLink>
       </div>
     </div>
   </div>
